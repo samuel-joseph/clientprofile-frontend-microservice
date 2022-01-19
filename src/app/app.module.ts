5e5e759d7859ef09a5d68a949aba166352a56a2a
arcanlte <pinangayjoel@gmail.com>
2022-01-19 12:11:37 -0500

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    PortfolioComponent,
+    TransactionHistoryComponent
   ],
   imports: [
     BrowserModule,
